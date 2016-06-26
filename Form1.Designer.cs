17ee5d05f1a6042d7c720a64e849137a74a6c220
Pafkens <mekukun@interia.pl>
2016-06-26 15:10:54 +0000

    Add files via upload
    poprawione

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Snake_Game
+{
+    partial class SnakeGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.SnakeScoreLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.spaceBarLabel = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.SnakeScoreLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 439);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(484, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(47, 17);
+            this.toolStripStatusLabel1.Text = "Punkty:";
+            // 
+            // SnakeScoreLabel
+            // 
+            this.SnakeScoreLabel.Name = "SnakeScoreLabel";
+            this.SnakeScoreLabel.Size = new System.Drawing.Size(13, 17);
+            this.SnakeScoreLabel.Text = "0";
+            // 
+            // spaceBarLabel
+            // 
+            this.spaceBarLabel.AutoSize = true;
+            this.spaceBarLabel.Location = new System.Drawing.Point(171, 225);
+            this.spaceBarLabel.Name = "spaceBarLabel";
+            this.spaceBarLabel.Size = new System.Drawing.Size(135, 13);
+            this.spaceBarLabel.TabIndex = 1;
+            this.spaceBarLabel.Text = "Wciśnij spację aby zacząć.";
+            // 
+            // SnakeGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.spaceBarLabel);
+            this.Controls.Add(this.statusStrip1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "SnakeGame";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.SnakeGame_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SnakeGame_KeyDown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel SnakeScoreLabel;
+        private System.Windows.Forms.Label spaceBarLabel;
+    }
+}
+
